29153544e8c0052c905cec729951d36a4a28ae7a
NikolayCh <nikolai_chervanyik@abv.bg>
2019-03-24 20:04:38 +0200

    Established database connection and products interface was introduced(select/deselect) methods

--- Fit4Life/Fit4Life/Data/Models/Cart.cs
+++ Fit4Life/Fit4Life/Data/Models/Cart.cs
@@ -8,6 +8,6 @@ namespace Fit4Life.Data.Models
 {
     class Cart:Product
     {
-       
+        
     }
 }
---
+++ Fit4Life/Fit4Life/Data/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Fit4Life.Data.Models
+{
+    abstract class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        Category supplement { get; set; }
+        public int Quantity { get; set; }
+        public string Weight { get; set; }
+        public double Price { get; set; }
+    }
+}
